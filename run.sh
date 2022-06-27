0be4cecbc0b7ead4b2dcd81adab26eb26e3977c8
Jonathan Wise <hanoldbuddy@Jonathans-MacBook-Pro.local>
2022-06-26 20:29:50 -0500

    rapid prototyping model ready.

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+# Remove after Prototyping
+docker cp run_app.sh $(cat contID.txt):/app
+# ------------------------------------------
 docker start $(cat contID.txt)
 docker exec -it $(cat contID.txt) /bin/sh run_app.sh
-docker stop $(cat contID.txt)
+# Uncomment after Prototyping
+# docker stop $(cat contID.txt)
+# -----------------------------
