6dedc4f7d82bcb09d50addb9a6d54dcd10d04a04
Jonathan Wise <hanoldbuddy@Jonathans-MBP.attlocal.net>
2022-07-02 09:41:56 -0500

    got main function in loop

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 docker cp ./app/run_app.sh $(cat contID.txt):/app/run_app.sh
 docker cp ./app/generate_key.sh $(cat contID.txt):/app/generate_key.sh
 docker cp ./app/encrypt_message.sh $(cat contID.txt):/app/encrypt_message.sh
+docker cp ./app/decrypt_message.sh $(cat contID.txt):/app/decrypt_message.sh
 # ------------------------------------------
 docker start $(cat contID.txt)
 docker exec -it $(cat contID.txt) /bin/sh run_app.sh
